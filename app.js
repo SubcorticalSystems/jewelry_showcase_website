8236781b52d7b4ad460bc44c0765da3032a91327
Matthew Moir <subcorticalsystems@gmail.com>
2023-01-23 15:22:24 -0500

    Initial commit before node implementation

---
+++ app.js
@@ -0,0 +1,8 @@
+/*Landing Page Javascript*/
+document.getElementById("explore-btn").onclick = function () {
+    window.location.href = "https://www.youtube.com/watch?v=4OXsxG-GIHI";
+}
+
+document.getElementById("project-btn").onclick = function () {
+    window.location.href = "https://www.youtube.com/watch?v=4OXsxG-GIHI";
+}
