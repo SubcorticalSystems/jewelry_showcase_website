59b63c509095bb44e93d65e368436711173576b1
Matthew Moir <subcorticalsystems@gmail.com>
2023-02-28 12:30:03 -0500

    Aesthetic Changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-/*Landing Page Javascript*/
+/*Button Functionality*/
 document.getElementById("explore-btn").onclick = function () {
     window.location.href = "project.html";
 }
-
 document.getElementById("project-btn").onclick = function () {
     window.location.href = "project.html";
 }
-
 document.getElementById("contact-btn").onclick = function () {
     window.location.href = "contact.html";
 }
+document.getElementById("about-btn").onclick = function () {
+    window.location.href = "about.html";
+}
 document.getElementById("home-btn").onclick = function () {
+    window.alert("HeLLOOW");
     window.location.href = "landing.html";
 }
